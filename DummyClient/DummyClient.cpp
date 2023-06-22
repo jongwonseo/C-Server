22844ba097a80d6c2f5129ec5cda09f6bde5e4c4
jongwonseo <whd7327@naver.com>
2023-06-22 11:22:01 +0900

    초기 세팅

---
+++ DummyClient/DummyClient.cpp
@@ -0,0 +1,8 @@
+﻿#include "pch.h"
+#include <iostream>
+
+int main()
+{
+    cout << "a" << endl;
+    HelloWorld();
+}
---
+++ ServerCore/pch.cpp
@@ -0,0 +1,8 @@
+﻿#include "pch.h"
+#include "CorePch.h"
+
+void HelloWorld()
+{
+	cout << "Hello Server" << endl;
+}
+
