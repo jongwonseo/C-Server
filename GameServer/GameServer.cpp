bdd4718c124b28fff13f092079a07e3007f7e186
jongwonseo <whd7327@naver.com>
2023-06-22 21:30:42 +0900

    condition_variable

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -7,36 +7,52 @@ mutex m;
 queue<int32> q;
 HANDLE handle;
 
+// 유저레벨(커널레벨X)(핸들과 차이점)
+condition_variable cv;
+
 void Producer()
 {
 	while (true)
 	{
+		// 1 락을 잡고
+		// 2 공유 변수 값을 수정
+		// 3 lock을 풀고
+		// 4 조건변수 통해 다른 쓰레드에게 통지
+
 		{
 			unique_lock<mutex> lock(m);
 			q.push(100);
 		}
 		
-		::SetEvent(handle); // signal로 바꿔라
+		
+		cv.notify_one(); // wait중인 쓰레드 1개 깨움
+		//::SetEvent(handle); // signal로 바꿔라
 		this_thread::sleep_for(100ms);
 	}
 }
 
 void Consumer()
 {
-	// signal상태면 진생, 아니면 잠듬
-	::WaitForSingleObject(handle, INFINITE);
 	
-	// bManualReset True일때 (CreateEvent 속성)
-	//::ResetEvent(handle);
+	//// signal상태면 진생, 아니면 잠듬
+	//::WaitForSingleObject(handle, INFINITE);
+	//
+	//// bManualReset True일때 (CreateEvent 속성)
+	////::ResetEvent(handle);
 	while (true)
-	{
+	{		
 		unique_lock<mutex> lock(m);
-		cout << "consumer" << endl;
-		if (q.empty() == false)
+		cv.wait(lock, []() {return q.empty() == false; });
+		// 1 락을 잡고
+		// 2 조건 확인, 만족하면 빠져나가서 이어서 실행
+		// 만족하지 않으면 lock을 풀어주고 대기 상태
+
+		
+		//while(q.empty() == false)
 		{
 			int32 data = q.front();
 			q.pop();
-			cout << data << endl;
+			cout << q.size() << endl;
 		}
 	}
 }
