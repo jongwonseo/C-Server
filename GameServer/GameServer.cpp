344847bc14572e4c1fbf943e342dec361d193023
jongwonseo <whd7327@naver.com>
2023-06-24 14:54:08 +0900

    RefCount

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -3,23 +3,85 @@
 #include "CorePch.h"
 #include "CoreMacro.h"
 #include "ThreadManager.h"
+#include "refCounting.h"
 
-CoreGlobal Core;
+class Wraith;
+class Missile;
 
-void ThreadMain()
+using WraithRef = TSharedPtr<Wraith>;
+using MissileRef = TSharedPtr<Missile>;
+
+class Wraith : public RefCountable
 {
-	while (true)
-	{
-		cout << "Hello I am thread..." << LThreadId << endl;
-		this_thread::sleep_for(1s);
-	}
-}
+public:
+	int _hp = 150;
+	int _posX = 0;
+	int _posY = 0;
+};
 
+using WraithRef = TSharedPtr<Wraith>;
+
+class Missile : public RefCountable
+{
+public:
+	void SetTarget(WraithRef target)
+	{
+		_target = target;
+		// 중간에 개입 가능
+		//target->AddRef();
+	}
+
+	bool Update()
+	{
+		if (_target == nullptr)
+			return true;
+
+		int posX = _target->_posX;
+		int posY = _target->_posY;
+
+		// TODO : 쫓아간다
+
+		if (_target->_hp == 0)
+		{
+			//_target->ReleaseRef();
+			_target = nullptr;
+			return true;
+		}
+
+		return false;
+	}
+
+	WraithRef _target = nullptr;
+};
 
 int main()
 {
-	for (int32 i = 0; i < 5; i++)
+	WraithRef wraith(new Wraith());
+	wraith->ReleaseRef();
+	MissileRef missile(new Missile());
+	missile->ReleaseRef();
+
+	missile->SetTarget(wraith);
+
+	// 레이스가 피격 당함
+	wraith->_hp = 0;
+	//delete Wraith;
+	//Wraith->ReleaseRef();
+	wraith = nullptr;
+
+	while (true)
 	{
-		GThreadManager->Launch(ThreadMain);
+		if (missile)
+		{
+			if (missile->Update())
+			{
+				//missile->ReleaseRef();
+				missile = nullptr;
+			}
+		}
 	}
+
+	//missile->ReleaseRef();
+	missile = nullptr;
+	//delete missile;
 }
