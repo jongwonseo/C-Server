179f06c523dbfeb4469525b1daa1f3f44e616b81
jongwonseo <whd7327@naver.com>
2023-06-26 23:34:55 +0900

    socket option

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -74,6 +74,67 @@ int main()
 
 		cout << "Send Data! Len = " << recvLen << endl;
 	}
+
+	// 옵션을 해석하고 처리할 주체
+	// 소켓 -> SOL_SOCKET
+	// IPv4 -> IPPROTO_IP
+	// TCP 프로토콜 -> IPPROTO_TCP
+
+	// SO_KEEPALIVE = 주기적으로 연결 상태 확인 여부(TCP only)
+	// 상대방이 소리소문없이 연결 끊는다면?
+	// 주기적으로 TCP 프로토콜 연결 상태 확인 -> 끊어진 연결 감지
+	bool enable = true;
+	::setsockopt(serverSocket, SOL_SOCKET, SO_KEEPALIVE,(char*)&enable, sizeof(enable));
+	
+	// SO_LINGER
+	// 송신 버퍼에 있는 데이터를 보낼 것인가? 날릴 것인가?
+	// onoff = 0이면 closesocket()이 바로 리턴, 아니면 linger초만큼 대기 (default 0)
+	//linger: 대기시간
+	LINGER linger;
+	linger.l_onoff = 1;
+	linger.l_linger = 5;
+	::setsockopt(serverSocket, SOL_SOCKET, SO_LINGER, (char*)&linger, sizeof(linger));
+
+	//Half-Close
+	// SD_SEND: send 막는다
+	// SD_RECEIVEL: recv 막는다
+	// SD_BOTH: 둘다 막는다
+	//::shutdown(serverSocket, SD_SEND); // 듣기만 하겠다
+	
+	//소켓리소스 반환
+	//::closesocket(serverSocket);
+
+	// SO_SNDBUF = 송신 버퍼 크기
+	// SO_RCVBUF = 수신 버퍼 크기
+	
+	int32 sendBufferSize;
+	int32 optionLen = sizeof(sendBufferSize);
+	::getsockopt(serverSocket, SOL_SOCKET, SO_SNDBUF, (char*)&sendBufferSize, &optionLen);
+	cout << "송신 버퍼 크기: " << sendBufferSize;
+
+	int32 recvBufferSize;
+	optionLen = sizeof(recvBufferSize);
+	::getsockopt(serverSocket, SOL_SOCKET, SO_RCVBUF, (char*)&sendBufferSize, &optionLen);
+	cout << "송신 버퍼 크기: " << recvBufferSize;
+
+
+	// SO_REUSEADDR
+	// IP주소 및 port 재사용
+	{	
+		bool enable = true;
+		::setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR, (char*)&enable, sizeof(enable));
+	}
+
+	// IPPROTO_TCP
+	// TCP_NODELAY = 네이글 알고리즘 작동 여부
+	// // 데이터가 충분히 크면 보내고, 그렇지 않으면 충분히 쌓일때까지 대기(회선낭비 줄이고, 효율적으로)
+	// 장) 작은패킷이 불필요하게 많이 생성되는 일을 방지
+	// 단) 반응 시간 손해
+	{
+		bool enable = true;
+		::setsockopt(serverSocket, IPPROTO_TCP, TCP_NODELAY, (char*)&enable, sizeof(enable));
+	}
+
 	// 윈속 종료
 	::WSACleanup();
 }
